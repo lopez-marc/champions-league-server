2596a6a44872e8ad420c755424283ce0b14f14c8
Marc Lppez <marclopeztorrent@gmail.com>
2022-03-01 11:46:30 +0100

    changed firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,16 @@ import admin from 'firebase-admin'
 
 import { readFile } from 'fs/promises'
 
-const serviceAccount = JSON.parse(
-  await readFile(new URL('./serviceAccountKey.json', import.meta.url))
-)
+// const serviceAccount = JSON.parse(
+//   await readFile(new URL('./serviceAccountKey.json', import.meta.url))
+// )
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(
+    JSON.parse(
+      Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii')
+    )
+  )
 })
 
 export default admin
